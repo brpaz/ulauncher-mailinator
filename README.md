02cb817a4c8fab77971ab3b55251b5c2a5ea3d8c
Bruno Paz <brunopaz@sapo.pt>
2018-06-10 17:27:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Ulauncher mailinator
+
+> [ulauncher](https://ulauncher.io/) extension that will generate a fake email address and open it for you.
+
+## Usage
+
+![demo](demo.gif)
+
+## Requirements
+
+* [ulauncher](https://ulauncher.io/)
+* Python >= 2.7
+* Faker library ```pip install faker```
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```https://github.com/brpaz/ulauncher-mailinator```
+ 
+
+## Development
+
+```
+git clone https://github.com/brpaz/ulauncher-mailinator
+cd ~/.cache/ulauncher_cache/extensions/ulauncher-mailinator
+ln -s <repo_location> ulauncher-mailinator
+```
+
+To see your changes, stop ulauncher and run it from the command line with: ```ulauncher -v```.
+
+## Related
+
+* [alfred-mailinator](https://github.com/AssafShalin/alfred-mailinator)
+
+## License 
+
+MIT
---
+++ main.py
@@ -0,0 +1,54 @@
+import logging
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+from faker import Faker
+import random
+
+logger = logging.getLogger(__name__)
+fake = Faker()
+
+MAILINATOR_URL = "https://www.mailinator.com/v2/inbox.jsp?zone=public&query=%s"
+
+class MailinatorExtension(Extension):
+
+    def __init__(self):
+        logger.info('init Mailinator Extension')
+        super(MailinatorExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+   
+        if (event.get_argument() is not None):
+            items.append(ExtensionResultItem(icon='images/icon.png',
+                                             name=event.get_argument(),
+                                             description="Press enter to open on mailinator.com",
+                                             on_enter=OpenUrlAction(
+                                                 MAILINATOR_URL % event.get_argument()
+                                             )))  
+        else:
+            email = extension.preferences.get(
+                "prefix") + fake.user_name() + str(random.randint(1000, 9999))
+
+            items.append(ExtensionResultItem(icon='images/icon.png',
+                                            name="%s@mailinator.com" % email,
+                                            description="Press enter to copy to the cliboard. Alt+Enter to open",
+                                            highlightable=False,
+                                            on_enter=CopyToClipboardAction(
+                                                email), 
+                                            on_alt_enter=OpenUrlAction(
+                                                MAILINATOR_URL % email)
+                                            ))
+
+
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+   MailinatorExtension().run()
