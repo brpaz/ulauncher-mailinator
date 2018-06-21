451e7fe2ed5be04fe83c04786450d2e91adcd65a
Gürkan Gür <gurkan.gur@innogames.com>
2018-06-21 13:01:01 +0200

    main.py: Some formatting and styling fixes

--- main.py
+++ main.py
@@ -12,43 +12,59 @@ import random
 logger = logging.getLogger(__name__)
 fake = Faker()
 
-MAILINATOR_URL = "https://www.mailinator.com/v2/inbox.jsp?zone=public&query=%s"
+MAILINATOR_URL = "https://www.mailinator.com/v2/inbox.jsp?zone=public&query="
+
 
 class MailinatorExtension(Extension):
 
     def __init__(self):
         logger.info('init Mailinator Extension')
         super(MailinatorExtension, self).__init__()
-        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(
+            KeywordQueryEvent,
+            KeywordQueryEventListener()
+        )
+
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         items = []
-   
+
         if (event.get_argument() is not None):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name=event.get_argument(),
-                                             description="Press enter to open on mailinator.com",
-                                             on_enter=OpenUrlAction(
-                                                 MAILINATOR_URL % event.get_argument()
-                                             )))  
+            items.append(
+                ExtensionResultItem(
+                    icon='images/icon.png',
+                    name=event.get_argument(),
+                    description="Press enter to open on mailinator.com",
+                    on_enter=OpenUrlAction(
+                        '{}{}'.format(MAILINATOR_URL, event.get_argument())
+                    )
+                )
+            )
         else:
-            email = extension.preferences.get(
-                "prefix") + fake.user_name() + str(random.randint(1000, 9999))
+            email = '{}{}{}'.format(
+                extension.preferences.get("prefix"),
+                fake.user_name(),
+                str(random.randint(1000, 9999))
+            )
 
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                            name="%s@mailinator.com" % email,
-                                            description="Press enter to copy to the cliboard. Alt+Enter to open",
-                                            highlightable=False,
-                                            on_enter=CopyToClipboardAction(
-                                                email), 
-                                            on_alt_enter=OpenUrlAction(
-                                                MAILINATOR_URL % email)
-                                            ))
-
+            items.append(
+                ExtensionResultItem(
+                    icon='images/icon.png',
+                    name='{}@mailinator.com'.format(email),
+                    description="Press enter to copy to the cliboard. Alt+Enter to open",
+                    highlightable=False,
+                    on_enter=CopyToClipboardAction(email),
+                    on_alt_enter=OpenUrlAction('{}{}'.format(
+                            MAILINATOR_URL,
+                            email
+                        )
+                    )
+                )
+            )
 
         return RenderResultListAction(items)
 
 if __name__ == '__main__':
-   MailinatorExtension().run()
+    MailinatorExtension().run()
